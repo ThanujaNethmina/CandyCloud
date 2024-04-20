38dcc0d6b454847c710f759dee99206030b4034c
Sarith-Samarakoon <128185621+Sarith-Samarakoon@users.noreply.github.com>
2024-04-20 15:16:50 +0530

    Header and Footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from "react";
-
+import React from "react";
 import "./App.css";
-import EmployeeCard from "./components/EmployeeCard";
-import EmployeeList from "./components/EmployeeList";
-import EmployeeLeave from "./components/EmployeeLeave";
+import Dashboard from "./components/Dashboard";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <EmployeeCard />
-      <EmployeeLeave />
+      <Dashboard />
     </>
   );
 }
